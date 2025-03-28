1fd5ed804299e85231b8a5afd852f0a675b4d0d1
Bhavarth Manikpuri <manikpuribhavarth@gmail.com>
2025-03-28 19:59:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
   }
 
   return (
-    <Router>
+    <Router basename="/Movie-Search-App">
       <Routes>
         <Route path="/" element={<Home films={films} handleRecent={handleRecent} showRecent={showRecent} recentViewed={recentViewed}/>} />
         <Route path="/movie-search/:id" element={<MovieSearch />} />
